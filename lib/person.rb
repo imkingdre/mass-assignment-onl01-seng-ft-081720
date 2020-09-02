8d1af330b80b4c40dda2681e4cca42b2245f9f86
Andre King <andreking1031@gmail.com>
2020-09-02 18:12:08 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color,
+  
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send (("#{key}="), value)
+    end 
+    
+  end
+  
 end
